838fe2daf6dd5c97b0af73398be413eacaf6915f
satendra <satendra.raj1241@gmail.com>
2021-09-30 00:46:27 +0530

    database changed

--- cht/settings.py
+++ cht/settings.py
@@ -82,22 +82,22 @@ WSGI_APPLICATION = 'cht.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'd927dmodpo4sfk',
-#         'USER' : 'eciodizttfslbg',
-#         'PASSWORD' : '8c20de955294c1596fc1fb40890aee1ee403834d98aeae2747d6893e7dd543b5',
-#         'HOST' : 'ec2-54-227-246-76.compute-1.amazonaws.com',
-#         'PORT' : '5432',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd927dmodpo4sfk',
+        'USER' : 'eciodizttfslbg',
+        'PASSWORD' : '8c20de955294c1596fc1fb40890aee1ee403834d98aeae2747d6893e7dd543b5',
+        'HOST' : 'ec2-54-227-246-76.compute-1.amazonaws.com',
+        'PORT' : '5432',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': 'mydatabase',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
